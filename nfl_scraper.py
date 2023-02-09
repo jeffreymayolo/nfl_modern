52ec6a5af0888ade10536c5cb15661964475714d
jeffreymayolo <jeffmayolo@g.harvard.edu>
2023-02-09 10:17:14 -0500

    Editing scraper to fix issue with winning/losing teams, rerunning the cleaning file and uploading the new clean dataset

--- nfl_scraper.py
+++ nfl_scraper.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-from single_game_scrape import get_game_stats
+from single_game_scrape import get_nfl_game_stats
 import random
 import numpy as np
 import re
@@ -9,14 +9,14 @@ import csv
 import requests
 
 # Open CSV file to write  webscraped data
-csv_file = open('nfl_scrape_test.csv', 'w')
+csv_file = open('nfl_scrape_home_scrape.csv', 'w')
 csv_writer = csv.writer(csv_file)
 
 # Boolean for if labels still need to be added to the csv
 labels_needed = True
 
 start_year = 2010
-end_year = 2012
+end_year = 2022
 # Loop through the year range specified
 for y in tqdm(range(start_year, end_year + 1)):
 
@@ -32,19 +32,23 @@ for y in tqdm(range(start_year, end_year + 1)):
         source_link = 'https://www.espn.com/nfl/schedule/_/week/' + week + '/year/' + year + '/seasontype/2'
         source = requests.get(source_link).text
         soup = BeautifulSoup(source, 'lxml')
+
+        all_teams = soup.find_all('span', class_ = 'Table__Team')
+        home_teams = all_teams[1::2]
         games = soup.find_all('td', class_ = 'teams__col Table__TD')
 
         # Loop through each game 
         for j, game in enumerate(games):
+            home_team = home_teams[j].find_all('a', class_ = 'AnchorLink')[1].text
             score = game.a.text
             score = score.split(' ')
 
             #Use try and except to get past weird situations like postponement
             try:
-                home_team = score[0]
-                home_score = score[1][:-1]
-                away_team = score[2]
-                away_score = score[3]
+                winning_team = score[0]
+                winning_score = score[1][:-1]
+                losing_team = score[2]
+                losing_score = score[3]
                 game_id = game.a['href'].split('=')[1]
             except Exception as e:
                 continue
@@ -60,7 +64,7 @@ for y in tqdm(range(start_year, end_year + 1)):
             # Write labels on first iteration
             if labels_needed:
                 statistic_list = list(game_stats.keys())
-                labels_list = ['year', 'week', 'home_team', 'home_score', 'away_team', 'away_score', 'game_id']
+                labels_list = ['year', 'week', 'home_team', 'winning_team', 'winning_score', 'losing_team', 'losing_score', 'game_id']
                 labels_list.extend(statistic_list)
                 csv_writer.writerow(labels_list)
                 labels_needed=False
@@ -71,7 +75,7 @@ for y in tqdm(range(start_year, end_year + 1)):
                 time.sleep(wait_time)
 
             #Store and write stats
-            write_stats = [year, week, home_team, home_score, away_team, away_score, game_id]
+            write_stats = [year, week, home_team, winning_team, winning_score, losing_team, losing_score, game_id]
             write_stats.extend(list(game_stats.values()))
             csv_writer.writerow(write_stats)
     
